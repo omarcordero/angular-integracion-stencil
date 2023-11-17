a89f0e0f662da90903df461aee60df427c3acfec
Omar Cordero <omar@cordero.com>
2023-11-17 16:40:22 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ComponentLibraryModule } from '@bricks/ng-components';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ComponentLibraryModule
   ],
   providers: [],
   bootstrap: [AppComponent]
